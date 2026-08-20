58a4b96590936d1bfbcc0c7ce3a181e009753c96
Arif <arifmalik16t@gmail.com>
2026-08-20 21:08:48 +0530

    Learn StrOutputParser and LCEL chaining

---
+++ output_parser.py/str_outputparser1.py
@@ -0,0 +1,35 @@
+# In this i learn how to use strOutputParser along with LCEL pipes to chain prompts and models.
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3.8-2.4T-A95B",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+# 1st prompt -> detailed report
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+# 2nd prompt -> summary
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text. /n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic': 'black hole'})
+
+print(result)
